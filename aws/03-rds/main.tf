9e1fdb4c68490ee3c277788774170cb2949b6686
CarolineBraga <caroline_braga@outlook.com.br>
2025-10-26 12:20:23 -0300

    terraform fmt

--- aws/03-rds/main.tf
+++ aws/03-rds/main.tf
@@ -1,8 +1,8 @@
 resource "aws_db_instance" "default_db" {
-  identifier          = var.db_identifier
-  allocated_storage   = var.db_allocated_storage
-  engine              = var.db_engine
-  instance_class      = var.db_instance_class
-  username            = var.db_username
-  password            = var.db_password
+  identifier        = var.db_identifier
+  allocated_storage = var.db_allocated_storage
+  engine            = var.db_engine
+  instance_class    = var.db_instance_class
+  username          = var.db_username
+  password          = var.db_password
 }
